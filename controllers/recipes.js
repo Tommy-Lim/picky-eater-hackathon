a53088c04272074a5f042380f37570c1ff796264
skylocke <mathematical.fiction@gmail.com>
2017-02-09 22:07:00 -0800

    testing example edamam api call via api/recipes/ root route

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,9 +4,37 @@ var models = require('../models/schemas');
 var mongoose = require('mongoose');
 var request = require('request');
 
+
 router.route('/')
 .get(function(req, res) {
-  res.send("success")
+  var healthLabels = []; // ex) [], ["Alcohol-Free"], ["Alcohol-Free", "Paleo"]
+  var dietLabels = ["High-Protein"]; // ex) [], ["High-Protein"], ["High-Protein", "Low-Carb"]
+
+  var healthParam = "";
+  healthLabels.forEach(function(filter) {
+    healthParam += "&health=" + filter.toLowerCase();
+  });
+
+  var dietParam = "";
+  dietLabels.forEach(function(filter) {
+    dietParam += "&diet=" + filter.toLowerCase();
+  });
+
+  var url = "https://api.edamam.com/search"
+    + "?app_id=" + process.env.APP_ID
+    + "&app_key=" + process.env.APP_KEY
+    + "&q=" + "chicken"
+    + "&from=" + 0
+    + "&to=" + 10
+    + healthParam
+    + dietParam;
+    // + calorieParam;
+    console.log(url)
+
+  request.get(url, function(error, response, body) {
+    // console.log(error)
+    res.send(body);
+  })
 });
 
 module.exports = router;
