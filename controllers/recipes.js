9acd2dd265c5d613d4c499e1930f3d4afe7f0c79
skylocke <mathematical.fiction@gmail.com>
2017-02-09 22:31:30 -0800

    testing edamam api call

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,7 +4,7 @@ var models = require('../models/schemas');
 var mongoose = require('mongoose');
 var request = require('request');
 
-
+// GET /api/recipes/:query
 router.route('/')
 .get(function(req, res) {
   var healthLabels = []; // ex) [], ["Alcohol-Free"], ["Alcohol-Free", "Paleo"]
@@ -23,6 +23,9 @@ router.route('/')
   var url = "https://api.edamam.com/search"
     + "?app_id=" + process.env.APP_ID
     + "&app_key=" + process.env.APP_KEY
+    // this is just to test the edamam API call
+    // for the rest of the query string, we can try serializing the form data
+    // to tack it on to the route as a :query via front-end
     + "&q=" + "chicken"
     + "&from=" + 0
     + "&to=" + 10
@@ -33,7 +36,7 @@ router.route('/')
 
   request.get(url, function(error, response, body) {
     // console.log(error)
-    res.send(body);
+    res.send(body); // we need to account for when the API returns an HTML body instead
   })
 });
 
--- controllers/saved.js
+++ controllers/saved.js
@@ -4,9 +4,24 @@ var models = require('../models/schemas');
 var mongoose = require('mongoose');
 var request = require('request');
 
+
+// GET /:userID/saved
+// return all recipes saved by user
 router.route('/')
 .get(function(req, res) {
   res.send("success")
 });
+
+
+// GET /:userID/saved/:recipeID
+// return a recipe object saved by user
+
+
+// GET /:userID/folders
+// return all recipe folders belonging to user
+
+
+// GET /:userID/folders/:folderID
+// return all recipes in a folder belonging to user
 
 module.exports = router;
