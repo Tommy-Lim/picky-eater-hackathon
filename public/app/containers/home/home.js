4fb5f513d692fe2c80e58ba7e72605b23b25e020
Tommy-Lim <tlim24@gmail.com>
2017-02-10 14:45:38 -0800

    redirect to profile after login

--- public/app/containers/home/home.js
+++ public/app/containers/home/home.js
@@ -17,6 +17,7 @@ function HomeCompCtrl($interval, $state, DataServices, AuthServices, Auth){
   DataServices.searchRecipes(homeComp.query).then(function(data){
     homeComp.results = data.data;
     console.log("searchComp.results: ", homeComp.results)
+    homeComp.query = "";
   })
 
 
