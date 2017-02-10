f250639554723e265b464ca3292eb2bbf8185b8e
Tommy-Lim <tlim24@gmail.com>
2017-02-10 14:58:06 -0800

    added the no results found condition on search results

--- public/app/containers/search/search.js
+++ public/app/containers/search/search.js
@@ -9,6 +9,7 @@ function SearchCompCtrl($state, DataServices){
   var searchComp = this;
 
   console.log("query: ", $state.params.query)
+  searchComp.query = $state.params.query;
 
   DataServices.searchRecipes($state.params.query).then(function(data){
     searchComp.results = data.data;
